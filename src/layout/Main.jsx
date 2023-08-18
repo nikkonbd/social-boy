e87dd018433778d1a702e3b6cb2a239e6ce8892f
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-08-19 02:44:40 +0600

    home pages commited

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,11 @@
 // import React from 'react';
 
+import { Outlet } from "react-router-dom";
+
 const Main = () => {
     return (
         <div>
-            <h2>This is Main</h2>
+            <Outlet></Outlet>
         </div>
     );
 };
