bfe0ffeb12c6f2893c15d6eafa643502c4010cdf
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-08-21 02:24:52 +0600

    added a order section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import OrderProgress from "../../Components/OrderProgress/OrderProgress";
+import OurMission from "../../Components/OurMission/OurMission";
 
 
 const Home = () => {
       return (
             <div>
                   <h2>this is home</h2>
+                  <OrderProgress></OrderProgress>
+                  <OurMission></OurMission>
             </div>
       );
 };
